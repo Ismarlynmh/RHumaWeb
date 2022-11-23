463753cfa47755ae94693c0680a4ef4b59eaa211
Ismarlin Mendoza Hernandez <90530848+Ismarlynmh@users.noreply.github.com>
2022-11-22 23:54:43 -0500

    commit

---
+++ DAL/Contexto.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+public class Contexto : DbContext
+{
+    public DbSet<Empleados> Empleados { get; set; }
+    public DbSet<Licencias> Licencias { get; set; }
+    public DbSet<Ausencias> Ausencias { get; set; }
+    public DbSet<Permisos> Permisos { get; set; }
+    public DbSet<Nomina> Nomina { get; set; }
+    public DbSet<Vacaciones> Vacaciones { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+
+}
